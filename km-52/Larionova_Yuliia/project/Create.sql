323e87a5b4e9b6cf7fbdb8caebf943fa5cb678b7
LarionovaJulia <larionovajul17@gmail.com>
2018-12-25 20:58:16 +0200

    Add sql files

---
+++ km-52/Larionova_Yuliia/project/Create.sql
@@ -0,0 +1,43 @@
+﻿CREATE TABLE users (
+    user_id INT NOT NULL,
+    email VARCHAR2(255) NOT NULL UNIQUE,
+    pass VARCHAR2(255) NOT NULL,
+    constraint PK_USER_ID primary key (user_id)
+);
+
+CREATE TABLE lists (
+    list_id INT NOT NULL,
+    list_name VARCHAR2(255) NOT NULL,
+    user_id INT NOT NULL,
+    constraint PK_LIST_ID primary key (list_id)
+);
+
+
+CREATE TABLE tasks (
+    task_id INT NOT NULL,
+    list_id INT NOT NULL,
+    task_name VARCHAR2(255) NOT NULL,
+    description VARCHAR2(255),
+    created_date DATE, 
+    due_date DATE,
+    checked INT DEFAULT 0,
+    constraint PK_TASK_ID primary key (task_id)
+)  ;
+
+
+alter table lists
+   add constraint FK_USER_ID foreign key (user_id)
+      references users(user_id);
+
+alter table tasks
+   add constraint FK_LIST_ID foreign key (list_id)
+      references lists(list_id);
+
+alter table users
+    add constraint user_email_content check (Regexp_like(email, '[a-z0-9.@]'));
+    
+alter table tasks
+    add constraint right_dates check (due_date >= created_date);
+    
+alter table tasks
+    add constraint right_checked check (Regexp_like(checked, '[0-1]'));
---
+++ km-52/Larionova_Yuliia/project/Populate.sql
@@ -0,0 +1,20 @@
+﻿INSERT INTO users(user_id, email, pass)
+VALUES (1, 'bla@bla.com', 'pass123');
+INSERT INTO users(user_id, email, pass)
+VALUES (2, 'larionovajul17@gmail.com', 'qwerty123');
+INSERT INTO users(user_id, email, pass)
+VALUES (3, 'petya@ukr.net', '12zxcv');
+
+INSERT INTO lists(list_id, list_name, user_id)
+VALUES (1, 'My list 1', 1);
+INSERT INTO lists(list_id, list_name, user_id)
+VALUES (2, 'My list 2', 1);
+INSERT INTO lists(list_id, list_name, user_id)
+VALUES (3, 'Calculus', 2);
+
+INSERT INTO tasks(task_id, list_id, task_name, description, created_date, due_date, checked)
+VALUES (1, 1, 'My task 1', 'smth intresting', '11-NOV-2018', '15-NOV-2018', 0);
+INSERT INTO tasks(task_id, list_id, task_name, description, created_date, due_date, checked)
+VALUES (2, 1, 'My task 2', 'smth new', '11-NOV-2018', '12-DEC-2018', 0);
+INSERT INTO tasks(task_id, list_id, task_name, description, created_date, due_date, checked)
+VALUES (3, 3, 'Prepare to test', 'It`s very important', '11-NOV-2018', '17-NOV-2018', 0);
